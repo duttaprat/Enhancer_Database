9c704438845091277f30df206de3949d14639d48
rekhasathian <98986875+rekhasathian@users.noreply.github.com>
2025-10-30 13:08:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -297,8 +297,8 @@ if page == "📊 Browse Data":
 
                 if filtered_display_df.empty:
                     st.warning(f"No results found for **'{search_query}'**.")
-                else:
-                    filtered_display_df = filtered_display_df[display_cols].drop_duplicates()
+                
+            filtered_display_df = filtered_display_df[display_cols].drop_duplicates()
 
             # --- Make ID clickable (fast HTML) ---
             if "ID" in filtered_display_df.columns:
